5f0b50010889f9fa2e797a0f23e7c91597b30622
Linuxe <angelren1220@gmail.com>
2023-03-28 19:27:17 -0400

    added hover action to reaction icons

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,8 +1,14 @@
-$('textarea').keyup(function() {
-  let characterCount = 140 - $(this).val().length;
-  let count = $('#count');
-  count.text(characterCount);
-  if (characterCount < 0) {
-    count.css('color', '#9c0607');
-  }
+$(document).ready(function() {
+  // count chars input in new tweet
+  $('textarea').keyup(function() {
+    let characterCount = 140 - $(this).val().length;
+    let count = $('#count');
+    count.text(characterCount);
+
+    // if overeeds the text limit, it turns red
+    if (characterCount < 0) {
+      count.css('color', '#9c0607');
+    }
+  });
+
 });
--- public/scripts/hover.js
+++ public/scripts/hover.js
@@ -1,8 +1,22 @@
-$('article').mouseover(function() {
-  const article = $(this);
-  article.css('box-shadow', '12px 12px 2px 1px #d8d8f0');
-});
-$('article').mouseout(function() {
-  const article = $(this);
-  article.css('box-shadow', 'none');
+$(document).ready(function() {
+  // hover for new tweet
+  $('article').mouseover(function() {
+    const article = $(this);
+    article.css('box-shadow', '12px 12px 2px 1px #d8d8f0');
+  });
+  $('article').mouseout(function() {
+    const article = $(this);
+    article.css('box-shadow', 'none');
+  });
+
+  // hover of tweet reaction icons
+  $('i').mouseover(function() {
+    const icon = $(this);
+    icon.css('color', '#d6a05e');
+  });
+  $('i').mouseout(function() {
+    const icon = $(this);
+    icon.css('color', '');
+  });
+
 });
