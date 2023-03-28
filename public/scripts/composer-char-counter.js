297d23f8a1d94ea55326a18b7467b2e712bf6c3c
Linuxe <angelren1220@gmail.com>
2023-03-28 12:37:06 -0400

    added color change to text count

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -6,4 +6,7 @@ $('textarea').keyup(function() {
   let characterCount = 140 - $(this).val().length;
   let count = $('#count');
   count.text(characterCount);
+  if (characterCount < 0) {
+    count.css('color', '#9c0607');
+  }
 });
