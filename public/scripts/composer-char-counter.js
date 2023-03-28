048a80618d818c592b527b605ae5531baeca366a
Linuxe <angelren1220@gmail.com>
2023-03-28 18:36:05 -0400

    added new tweet container

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,12 +1,8 @@
-$(document).ready(function() {
-  console.log("ready");
-});
-
 $('textarea').keyup(function() {
   let characterCount = 140 - $(this).val().length;
   let count = $('#count');
   count.text(characterCount);
   if (characterCount < 0) {
-    count.attr('id', 'new-color');
+    count.attr('id', 'over-size');
   }
 });
