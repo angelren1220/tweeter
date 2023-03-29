335750351d2d396d42c3f5dbd6aedb1550b5e7ff
Linuxe <angelren1220@gmail.com>
2023-03-29 14:13:19 -0400

    implemented createTweetElement

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,45 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(document).ready(function() {
+  const createTweetElement = function(data) {
+    const $tweet = $(`<article class="tweet">
+    <script src="./scripts/hover.js"></script>
+    <header class="tweet-header">
+      <img class="tweet-header" src=${data.user.avatars}>
+      <h4>${data.user.name}</h4>
+    </header>
+    <p class="posted-tweet">${data.content.text}</p>
+    <footer class="posted-tweet">
+      <label>${data.content.created_at}</label>
+      <div class="tweet-reactions">
+        <i class="fa-solid fa-flag"></i>
+        <script src="./scripts/hover.js"></script>
+        <i class="fa-solid fa-retweet"></i>
+        <script src="./scripts/hover.js"></script>
+        <i class="fa-solid fa-heart"></i>
+        <script src="./scripts/hover.js"></script>
+      </div>
+    </footer>
+    </article>`);
+    return $tweet;
+  };
+
+  const tweetData = {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png",
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  };
+
+  const $tweet = createTweetElement(tweetData);
+
+  // Test / driver code (temporary)
+  // console.log($tweet); // to see what it looks like
+  $('#tweet-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+});
