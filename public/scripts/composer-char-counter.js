6d0bcea76bd5b4c2c95284bb51ef9504e5c5166d
Linuxe <angelren1220@gmail.com>
2023-03-29 11:08:06 -0400

    fixed bug count color does not change back

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,12 +2,14 @@ $(document).ready(function() {
   // count chars input in new tweet
   $('textarea').keyup(function() {
     let characterCount = 140 - $(this).val().length;
-    let count = $('#count');
+    let count = $('output');
     count.text(characterCount);
 
     // if overeeds the text limit, it turns red
     if (characterCount < 0) {
       count.css('color', '#9c0607');
+    } else {
+      count.css('color', '');
     }
   });
 
